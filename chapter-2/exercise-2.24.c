5f45a4795784bf1792e3c54ba616e790e6eb635d
hammamzagh <84284843+hammamzagh@users.noreply.github.com>
2021-05-16 19:57:06 +0300

    (Largest Integers) Write a program that reads in three integers and then determines and prints the largest in the group.

--- chapter-2/exercise-2.24.c
+++ chapter-2/exercise-2.24.c
@@ -1,9 +1,6 @@
-// Exercises 2.21
-// (Odd or Even) Write a program that reads an integer and determines and prints
-// whether it is odd or even. [Hint: Use the remainder operator. Any even number
-// is a multiple of two. Any multiple of two leaves a remainder of zero when
-// divided by 2.]
-
+ (Largest Integers) Write a program that reads in three integers and then determines and
+prints the largest in the group. 
+  
 #include <stdio.h>
 
 int main() {
